584397d52298781d0bb5766eb9deaaf03835a82e
kate <kompp@yandex.ru>
2019-08-14 19:23:02 +0200

    add: opportunity to save essay to file

---
+++ js-script.js
@@ -0,0 +1,18 @@
+function download(type) {
+    var file = new Blob([$("#count-words").val()], {type: type});
+    filename = $("#filename").val() + ".txt";
+    if (window.navigator.msSaveOrOpenBlob) // IE10+
+        window.navigator.msSaveOrOpenBlob(file, filename);
+    else { // Others
+        var a = document.createElement("a"),
+                url = URL.createObjectURL(file);
+        a.href = url;
+        a.download = filename;
+        document.body.appendChild(a);
+        a.click();
+        setTimeout(function() {
+            document.body.removeChild(a);
+            window.URL.revokeObjectURL(url);  
+        }, 0); 
+    }
+}
